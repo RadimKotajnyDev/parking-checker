4c9fd1b397058dfd043d6dab799803e98a48f26d
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-05-03 10:12:15 +0200

    broken :)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {ChakraProvider} from '@chakra-ui/react'
+import theme from '../styles/ChakraTheme'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
