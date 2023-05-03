8a0f24689958b65162bfa8453fe0c7a25614b451
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-05-03 12:42:55 +0200

    sem 5/3

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 - arduino
 ### Description
 - This project will simply check a parking spot if it's:
-    - Free (Green)
-    - Occupied (Red)
-    - Unknown state (Purple)
+    - Free (Green) - corresponding to value 1
+    - Occupied (Red) - corresponding to value 2
+    - Unknown state (Purple) - corresponding to value 3
 - Progress of getting data
     - Arduino -> LoraWAN -> Cloud Server -> API -> NextJS browser Client
     - Each change will take up apx. to 5 seconds.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import {Inter} from 'next/font/google'
+import {Spot, Convert} from "./spot"
 import {
   Grid,
   GridItem,
@@ -7,14 +8,36 @@ import {
   Center,
   Flex
 } from "@chakra-ui/react";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const initialSpot: Spot = {
+    id : "",
+    value : 0
+  }
+  const url = "./test.json"
+
+  const [Spots, setSpot] = useState([initialSpot]);
+
+  useEffect(() => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          setSpot(JSON.parse(data));
+    })
+  })
+
   return (
     <>
       <Center>
         <Flex flexDirection="column" align="center" justify="center">
           <Heading>Parking checker system</Heading>
           <Grid>
+            {
+              Spots.map((spot): any => {
+                <GridItem key={spot.id}>{spot.value}</GridItem>
+              })
+            }
             <GridItem h={10} w={10} bg="teal.700" borderRadius="md"/>
           </Grid>
         </Flex>
