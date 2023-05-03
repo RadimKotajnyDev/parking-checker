5b0ea35ee05f0c01f580f910215513d41354d482
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-05-03 13:07:48 +0200

    api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,44 @@
-import Image from 'next/image'
-import {Inter} from 'next/font/google'
 import {Spot, Convert} from "./spot"
-import {
-  Grid,
-  GridItem,
-  Heading,
-  Center,
-  Flex
-} from "@chakra-ui/react";
+import {Center, Flex, Grid, GridItem, Heading} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then(r => r.json())
 
 export default function Home() {
-  const initialSpot: Spot = {
-    id : "",
-    value : 0
-  }
-  const url = "./test.json"
+    const initialSpot: Spot = {
+        id: "",
+        value: 0
+    }
+    const url = "./test.json"
 
-  const [Spots, setSpot] = useState([initialSpot]);
+    const [Spots, setSpot] = useState([initialSpot]);
 
-  useEffect(() => {
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          setSpot(JSON.parse(data));
-    })
-  })
+    const { data, error } = useSWR(url, fetcher)
 
-  return (
-    <>
-      <Center>
-        <Flex flexDirection="column" align="center" justify="center">
-          <Heading>Parking checker system</Heading>
-          <Grid>
-            {
-              Spots.map((spot): any => {
-                <GridItem key={spot.id}>{spot.value}</GridItem>
-              })
-            }
-            <GridItem h={10} w={10} bg="teal.700" borderRadius="md"/>
-          </Grid>
-        </Flex>
-      </Center>
-    </>
-  )
+    useEffect(() => {
+        console.log(error)
+        setSpot(Convert.toSpot(data))
+    }, [data, error])
+
+    return (
+        <>
+            <Center>
+                <Flex flexDirection="column" align="center" justify="center">
+                    <Heading>Parking checker system</Heading>
+                    <Grid>
+                        {
+                            Spots.map((spot): any => {
+                                const {id, value} = spot
+                                return (
+                                    <GridItem key={id}>{value}</GridItem>
+                                )
+                            })
+                        }
+                        <GridItem h={10} w={10} bg="teal.700" borderRadius="md"/>
+                    </Grid>
+                </Flex>
+            </Center>
+        </>
+    )
 }
