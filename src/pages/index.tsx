1ad87a494cf96586a9c0026dd64a94ed6238d701
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2023-05-04 11:39:12 +0200

    added legend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,15 @@
-import {Center, Flex, Grid, GridItem, Heading, Spinner} from "@chakra-ui/react";
+import {
+    Center, Flex, Grid, GridItem, Heading, Spinner,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+    PopoverAnchor, Button, UnorderedList, ListItem, Box, Text, Spacer
+} from "@chakra-ui/react";
 import {useEffect, useState} from "react";
 import useSWR from "swr";
 
@@ -15,8 +26,8 @@ function uniqueLetterCount(Spots: Spot[]): number {
     Spots.map((spot: Spot): void => {
         newArray.push(spot.id[0])
     })
-    let uniqueChars: String[] = [...new Set(newArray)]
-    return uniqueChars.length
+    //let uniqueChars: String[] = [...new Set(newArray)]
+    //return uniqueChars.length
 }
 
 function maxLetterCount(Spots: Spot[], letter: string): number {
@@ -53,7 +64,7 @@ export default function Home() {
         if (data) {
             data.map((item: Spot) => {
                 if(item) {
-                    console.log(item)
+                    //console.log(item)
                     Spots.push(item)
                 }
             })
@@ -63,12 +74,36 @@ export default function Home() {
     }, [data, error])
 
     let color: string
-    let sactors = [...Array(uniqueLetterCount(Spots)).keys()]
+    //let sactors = [...Array(uniqueLetterCount(Spots)).keys()]
     return (
         <>
             <Center>
                 <Flex flexDirection="column" align="center" justify="center">
                     <Heading>Parking checker system</Heading>
+
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button>Legend</Button>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader>Legend</PopoverHeader>
+                            <PopoverBody>
+                                <Flex flexDirection="row" my={2}>
+                                    <Box p={3} bg="teal.500" borderRadius="md"></Box>&nbsp;<Text>is free</Text>
+                                </Flex>
+                                <Flex flexDirection="row" my={2}>
+                                    <Box p={3} bg="red.500" borderRadius="md"></Box>&nbsp;<Text>is occupied</Text>
+                                </Flex>
+                                <Flex flexDirection="row" my={2}>
+                                    <Box p={3} bg="purple.500" borderRadius="md"></Box>&nbsp;<Text>is unknown</Text>
+                                </Flex>
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Popover>
+
+
                     <p>{maxLetterCount(Spots, "b")}</p>
                     <p>{uniqueLetterCount(Spots)}</p>
                     <Flex>
--- src/styles/ChakraTheme.tsx
+++ src/styles/ChakraTheme.tsx
@@ -3,6 +3,7 @@ import "@fontsource/open-sans";
 
 const Theme = extendTheme({
   fonts: {
+    Heading: `'Open Sans', sans-serif`,
     body: `'Open Sans', sans-serif`,
   },
 })
