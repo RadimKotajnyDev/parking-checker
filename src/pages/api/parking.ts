f5eef326b1173dabed7d267c75ab9dab4be760e1
ValentinyFilip <80767029+ValentinyFilip@users.noreply.github.com>
2023-05-15 12:16:10 +0200

    changes

--- src/pages/api/parking.ts
+++ src/pages/api/parking.ts
@@ -1,17 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Spot } from "@/pages/spot";
-import testJson from '../../../public/test.json' assert {type: 'json'}
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Spot[]>
+  res: NextApiResponse
 ) {
-  const { query, method } = req
-  const status = parseInt(query.data as string, 10)
-  const EUI = query.EUI as string
+  const { query, method, body } = req
+  const status = parseInt(body.data as string, 10)
+  const EUI = body.EUI as string
   switch (method) {
     case "GET":
       // @ts-ignore
@@ -22,18 +21,19 @@ export default async function handler(
         }
       })
       res.status(200).json(allSpots)
+      res.end()
       break
     case "POST":
-      console.log(query, status, EUI)
-      // const spot: Spot = await prisma.spot.update({
-      //   where: {
-      //     EUI: EUI,
-      //   },
-      //   data: {
-      //     status: status,
-      //   },
-      // })
-      res.status(200).json(testJson)
+      const spot: Spot = await prisma.spot.update({
+        where: {
+          EUI: EUI,
+        },
+        data: {
+          status: status,
+        },
+      })
+      res.status(200)
+      res.end()
       break
   }
 }
